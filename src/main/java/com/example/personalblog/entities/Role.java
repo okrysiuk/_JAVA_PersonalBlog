3d19d7e0af35c373ee74168265f9f145221277fe
okrysiuk <oleksandr.krysiuk@gmail.com>
2020-07-31 15:41:43 +0300

    security done

---
+++ src/main/java/com/example/personalblog/entities/Role.java
@@ -0,0 +1,5 @@
+package com.example.personalblog.entities;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/example/personalblog/repositories/UserRepo.java
@@ -0,0 +1,9 @@
+package com.example.personalblog.repositories;
+
+
+import com.example.personalblog.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
